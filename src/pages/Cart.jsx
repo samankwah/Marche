309ade8975c060873109028c6c1b8d5c62246140
1amankwah <stephen.amankwah@meteo.gov.gh>
2024-11-26 08:52:40 +0000

    refactor: deployment

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,24 @@
-import { Breadcrumb, CartItems } from "../components";
-import { useCartContext } from "../context/cart/cart_context";
-import { Empty_cart } from "../components";
+// import { Breadcrumb, CartItems } from "../components";
+// import { useCartContext } from "../context/cart/cart_context";
+// import { Empty_cart } from "../components";
+
+// const Cart = () => {
+//   const { cart } = useCartContext();
+//   if (cart.length === 0) return <Empty_cart />;
+//   return (
+//     <>
+//       <Breadcrumb title="cart" />
+//       <CartItems />
+//     </>
+//   );
+// };
+
+// export default Cart;
+
+import React from "react";
 
 const Cart = () => {
-  const { cart } = useCartContext();
-  if (cart.length === 0) return <Empty_cart />;
-  return (
-    <>
-      <Breadcrumb title="cart" />
-      <CartItems />
-    </>
-  );
+  return <div>Cart</div>;
 };
 
 export default Cart;
